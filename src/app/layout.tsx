60da3b26ace9a35eb8f4bb6013e6e05e76017fd0
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2025-04-16 19:02:35 -0600

    Add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,57 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'South Jordan Smart Home',
+  title: "South Jordan Smart Home",
   description:
-    'South Jordan Smart Home Services offers expert TV mounting and smart home automation, including smart lighting, thermostats, and doorbell cameras. Serving South Jordan and Daybreak with professional installation and seamless integration for Alexa, Google, and Apple HomeKit.',
-}
+    "South Jordan Smart Home Services offers expert TV mounting and smart home automation, including smart lighting, thermostats, and doorbell cameras. Serving South Jordan and Daybreak with professional installation and seamless integration for Alexa, Google, and Apple HomeKit.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
+      <head>
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
-  )
+  );
 }

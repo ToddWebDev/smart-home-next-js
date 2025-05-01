053b1f6313e44273d9ffb58170724befc7a53cef
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2025-04-30 19:22:11 -0600

    Update favicon

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,7 +1,11 @@
 export const GA_MEASUREMENT_ID = "G-CMMYTVP534";
 
 export const pageview = (url) => {
-  window.gtag("config", GA_MEASUREMENT_ID, {
-    page_path: url,
-  });
+  if (typeof window !== "undefined" && typeof window.gtag === "function") {
+    window.gtag("config", GA_MEASUREMENT_ID, {
+      page_path: url,
+    });
+  } else {
+    console.warn("window.gtag is not defined");
+  }
 };

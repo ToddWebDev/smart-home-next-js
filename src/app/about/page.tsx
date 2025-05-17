3af89e9a518b62fb970a625bb80a96a19d740f6e
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2025-05-16 18:05:31 -0600

    Update button text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -65,7 +65,7 @@ export default function AboutPage() {
           href="/contact"
           className="inline-block rounded-lg bg-blue-700 px-6 py-3 font-semibold text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Request a free quote →
+          Request a free estimate →
         </Link>
       </div>
     </>
